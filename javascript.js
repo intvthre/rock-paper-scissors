c05d4f9bc9bc7b2d0af115761ec12c6906b30081
intvthre <170500856+intvthre@users.noreply.github.com>
2024-05-28 10:47:38 +0200

    Add score variables, add playRound conditionals

--- javascript.js
+++ javascript.js
@@ -39,5 +39,66 @@ function getHumanChoice () {
 
 }
 
+// Declare the player score variables
+
+// Create variables 'humanScore' and 'computerScore'
+// Initialize variables with value of 0
+
+let humanScore = 0;
+let computerScore = 0;
 
 
+// Write the logic to play a single round
+
+// "Function" {
+// (Input = two parameters "humanChoice" and "computerChoice" taking
+// "getHumanChoice" and "getComputerChoice" as arguments, and keeping score;
+//  pass values(arguments) to function by passing value from previous functions)
+// 'humanChoice' parameter 'toLowerCase'
+// Code 'console.log for 'playRound' to declare round winner
+// 'humanScore' and 'computerScore' incrementing based on round winner
+//}
+
+function playRound (humanChoice, computerChoice) {
+
+    if (humanChoice() === "rock", computerChoice() === "rock" ) {
+        console.log("It's a draw!");
+    }   
+
+    else if (humanChoice() === "rock", computerChoice() === "paper" ) {
+        console.log("You lose!");
+    }   
+
+    else if (humanChoice() === "rock", computerChoice() === "scissors" ) {
+        console.log("You win!");
+    }   
+
+    else if (humanChoice() === "paper", computerChoice() === "rock" ) {
+        console.log("You win!");
+    }   
+
+    else if (humanChoice() === "paper", computerChoice() === "paper" ) {
+        console.log("It's a draw!");
+    }   
+
+    else if (humanChoice() === "paper", computerChoice() === "scissors" ) {
+        console.log("You lose!");
+    }
+
+    else if (humanChoice() === "scissors", computerChoice() === "rock" ) {
+        console.log("You lose!");
+    }
+
+    else if (humanChoice() === "scissors", computerChoice() === "paper" ) {
+        console.log("You win!");
+    }   
+
+    else if (humanChoice() === "scissors", computerChoice() === "scissors" ) {
+        console.log("It's a draw!");
+    }   
+}
+
+const humanChoice = getHumanChoice.toLowerCase();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
